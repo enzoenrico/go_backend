05ae0ae93c8e89c14b1e30cba797979e3449a1ba
Enzo Enrico <e.enrico2005@gmail.com>
2024-10-09 12:06:32 -0300

    'codecrafters'

--- app/responses/responses.go
+++ app/responses/responses.go
@@ -5,8 +5,13 @@ import (
 )
 
 func RespondOK(connection net.Conn) bool {
-	_, err := connection.Write([]byte("HTTP/1.1 200 OK\r\n"))
-	if err != nil{
+	ok_response := "HTTP/1.1 200 OK\r\n"
+
+	crlf := "\r\n"
+
+	_, err := connection.Write([]byte(ok_response + crlf))
+
+	if err != nil {
 		return false
 	}
 	return true
