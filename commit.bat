49d1702222e9d404d0ebc2045ba27eb0d1e15e04
Enzo Enrico <e.enrico2005@gmail.com>
2024-10-09 15:07:45 -0300

    Changed files:  commit.bat

--- commit.bat
+++ commit.bat
@@ -6,7 +6,7 @@ git add .
 REM Capture the list of changed files
 SET "changed_files="
 FOR /F "delims=" %%f IN ('git diff --cached --name-only') DO (
-    SET "changed_files=!changed_files! %%f"
+    SET "changed_files= %%f"
 )
 
 REM Check if there are any changed files
