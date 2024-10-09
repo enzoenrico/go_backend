4899d86db711d97cb70fc491cebbdd80970f5b64
Enzo Enrico <e.enrico2005@gmail.com>
2024-10-09 12:39:22 -0300

    'codecrafters'

--- app/utils/utils.go
+++ app/utils/utils.go
@@ -2,12 +2,12 @@ package utils
 
 import "net/http"
 
-func ExtractPath(response http.Response) string{
+func ExtractPath(response *http.Response) string {
 	return response.Request.URL.Path
 }
 
-func ValidPath(path string) bool{
-	if path == "/"{
+func ValidPath(path string) bool {
+	if path == "/" {
 		return true
 	}
 	return false
