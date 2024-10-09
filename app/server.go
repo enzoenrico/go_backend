1d812cecad0e163a085e795402fc2000df5094ec
Enzo Enrico <e.enrico2005@gmail.com>
2024-10-09 12:00:30 -0300

    'codecrafters'

--- app/server.go
+++ app/server.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"net"
 	"os"
 )
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	l, err := http.ListenAndServe(":8080", nil)
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
