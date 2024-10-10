4be701226a53ce9cc098d3454d868837dd0fc97f
Enzo Enrico <e.enrico2005@gmail.com>
2024-10-10 11:39:54 -0300

    Updated:  app/utils/router.go

--- app/utils/router.go
+++ app/utils/router.go
@@ -10,46 +10,43 @@ import (
 // needs a connection and a path for a pattern string
 type HandlerFunc func(net.Conn, string)
 
-//single route properties
+// single route properties
 type RouteEntry struct {
-	Path string
-	Method string 
-	Handler http.HandlerFunc 
+	Path    string
+	Method  string
+	Handler http.HandlerFunc
 }
 
-//stores all of the paths with their perspective methods and handlers
-type Router struct{
+// stores all of the paths with their perspective methods and handlers
+type Router struct {
 	routes []RouteEntry
 }
 
-func (rtr *Router) Route(method, path string, handlerFunc http.HandlerFunc){
-	e:= RouteEntry{
-		Method: method,
-		Path: path,
+func (rtr *Router) Route(method, path string, handlerFunc http.HandlerFunc) {
+	e := RouteEntry{
+		Method:  method,
+		Path:    path,
 		Handler: handlerFunc,
 	}
 	rtr.routes = append(rtr.routes, e)
 }
 
-
 func (re *RouteEntry) Match(r *http.Request) bool {
-	if r.Method != re.Method || r.URL.Path != re.Path{
+	if r.Method != re.Method || r.URL.Path != re.Path {
 		return false
 	}
 	return true
 }
 
-
 func (rtr *Router) ServeHTTP(r *http.Request, conn net.Conn) {
-	for _, e := range rtr.routes{
+	for _, e := range rtr.routes {
 		match := e.Match(r)
-		if !match{
+		if !match {
 			continue
 		}
+		responses.RespondWithBody("hi", conn)
 		return
 	}
 
 	responses.NotFound(conn)
 }
-
-
